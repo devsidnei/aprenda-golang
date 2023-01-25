985c201d9634a2cefdc616ffe8e200e9e1a5d09b
Sidnei Simeão <sidnei.simeao@vilanova.com.br>
2023-01-25 12:42:54 -0300

    11 - Estrutura de Controler IF

--- 11-EstruturaDeControle/estrutura-de-controle.go
+++ 11-EstruturaDeControle/estrutura-de-controle.go
@@ -7,6 +7,8 @@ import (
 )
 
 func main() {
+	// Com IF init é possivel inicializar variavel de escopo
+	// Visiveis somente dentro do escopo do IF no caso da condição ser atendida
 	if myNumber := ramdomNumber(0, 100); myNumber < 50 {
 		fmt.Println("É possivel inicializar variavel dentro da condicional do IF")
 	}
